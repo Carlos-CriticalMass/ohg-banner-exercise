1c6b8da84c96cee7c196a38a194c09bfead27eb2
Carlos Bonilla <carlos.bonilla@hangarww.com>
2022-06-07 11:31:17 -0600

    refactor/main.js update
    - Separating the ISI code and the banner animation
     This allows us to disable all isi js just changing a variable.
     The main work for a banner is related to the animation, now the animation code is at the top of the file.

--- src/banner_list/728x90/js/02main.js
+++ src/banner_list/728x90/js/02main.js
@@ -5,34 +5,80 @@
  * start animating
  *
  **/
+const animation = {
+    initVariables: function() {
+        this.animationFinished = false;
+        this.tl = new TimelineMax();
+    },
+    init: function() {
+        const self = this;
+        self.initVariables();
 
-AppBanner.fn.anima = function() {
-    // isi clone, NOTE: if you want to see a copy of the ISI next to the banner uncomment the follow line
-    // $('#isi').clone().addClass('uat').attr('id', 'isi-clone').appendTo('body');
+        // Assign timeline to window to be able to test.
+        window.tl = self.tl;
 
-    // Variables
-    const tl = new TimelineMax();
-    const placeholder = $('.animation-placeholder');
-    const isi = $('.isi');
-    const isiMain = $('.isi-main');
-    const isiWrapper = $('.isi_wrapper');
-    const mainExit = $('#mainExit');
-    const initialScrollSpeed = 90000;
-    const scrollToTop = true; // Set to false if you do not need to scroll to the top when the auto scroll finished
-    let myScroll = null;
-    let scrollBar = null;
-    let scrollSpeed = 0;
-    let scrolledPercentage = 0;
-    let isiHeight = 0;
-    let isiFinished = false;
-    let animationFinished = false;
-    let scrollWrapHeight = isiWrapper.clientHeight;
-    // Assign timeline to window to be able to test.
-    window.tl = tl;
+        function finishedAnimation() {
+            self.animationFinished = true;
+            if (isi.config.enabled) {
+                isi.startScroll();
+            }
+        }
 
-    // Scroll init function. Keep disable options as they
-    function initScrollBars() {
-        myScroll = new IScroll('.isi_wrapper', {
+        //
+        // Timeline Animation
+        //
+        self.tl.addLabel('frame1', '+=0.5')
+            .from(placeholder, 0.6, {
+                x: 500,
+            }, 'frame1')
+            .add(function() {
+                finishedAnimation();
+            }, 'frame1+=3');
+    },
+};
+
+const isi = {
+    config: {
+        enabled: true,
+        scrollToTop: true,
+        isiClone: false,
+    },
+    initVariables: function() {
+        this.animationFinished = false;
+        this.isiMain = $('.isi-main');
+        this.scrollBar = $('.iScrollVerticalScrollbar');
+        this.isiFinished = false;
+        this.myScroll = null;
+    },
+
+    init: function() {
+        const self = this;
+        self.initVariables();
+
+        if (self.config.isiClone) {
+            // isi clone, NOTE: if you want to see a copy of the ISI next to the banner set the isiClone config to true
+            $('#isi').clone().addClass('uat').attr('id', 'isi-clone').appendTo('body');
+        }
+
+        self.isiMain.mouseenter(function() {
+            self.stopScroll();
+        });
+
+        self.isiMain.mouseleave(function() {
+            if (animation.animationFinished) {
+                if (!self.isiFinished) {
+                    self.startScroll();
+                }
+            }
+        });
+
+        self.initScrollBars();
+    },
+
+    initScrollBars: function() {
+        const self = this;
+        // Scroll init function. Keep disable options as they
+        self.myScroll = new IScroll('.isi_wrapper', {
             scrollbars: 'custom',
             interactiveScrollbars: true,
             resizeScrollbars: false,
@@ -44,94 +90,72 @@ AppBanner.fn.anima = function() {
             disableMouse: false,
         });
 
-        window.myScroll = myScroll;
+        self.myScroll.on('scrollStart', function() {
+            if (animation.animationFinished) {
+                if (self.myScroll.isAnimating) {
+                    self.stopScroll();
+                }
+            }
+        });
 
-        scrollBar = $('.iScrollVerticalScrollbar');
-    }
+        self.myScroll.on('scrollEnd', function() {
+            if (!self.config.scrollToTop) return;
+            if (myScroll.maxScrollY >= myScroll.y) {
+                self.stopScroll();
+                setTimeout(function() {
+                    self.myScroll.scrollTo(0, 0, 2000);
+                }, 3000);
+            }
+        });
 
-    function scrollSetUp(e) {
-        myScroll.scrollBy(0, 0, 1, {
+        window.myScroll = self.myScroll;
+
+        self.scrollBar.mouseenter(function() {
+            self.scrollSetUp();
+        });
+    },
+    scrollSetUp: function() {
+        this.myScroll.scrollBy(0, 0, 1, {
             fn: function(k) {
                 return k;
             },
         });
-    }
+    },
+    startScroll: function() {
+        const self = this;
+        const initialScrollSpeed = 90000;
+        const scrollWrapHeight = $('.isi_wrapper').outerHeight();
+        const isiHeight = -1 * ($('.isi').outerHeight() - scrollWrapHeight);
+        const scrolledPercentage = self.myScroll.y * 100 / isiHeight;
+        const scrollSpeed = initialScrollSpeed - (initialScrollSpeed * (scrolledPercentage / 100));
 
-    function finishedAnimation() {
-        animationFinished = true;
-        startScroll();
-    }
-
-    function startScroll() {
-        scrollWrapHeight = $('.isi_wrapper').outerHeight(),
-        isiHeight = -1 * (isi.outerHeight() - scrollWrapHeight),
-        scrolledPercentage = myScroll.y * 100 / isiHeight,
-        scrollSpeed = initialScrollSpeed - (initialScrollSpeed * (scrolledPercentage / 100));
-
-        myScroll.refresh();
+        self.myScroll.refresh();
         setTimeout(function() {
             if (scrolledPercentage >= 100) {
-                isiFinished = true;
+                self.isiFinished = true;
             }
-            myScroll.scrollTo(0, isiHeight, scrollSpeed, {
+            self.myScroll.scrollTo(0, isiHeight, scrollSpeed, {
                 fn: function(k) {
                     return k;
                 },
             });
         }, 300);
+    },
+    stopScroll: function() {
+        self.myScroll.isAnimating = false; // stop animation
+    },
+};
+
+
+AppBanner.fn.anima = function() {
+    // Variables
+    const mainExit = $('#mainExit');
+
+    animation.init();
+
+    if (isi.config.enabled) {
+        isi.init();
     }
-
-    function stopScroll() {
-        myScroll.isAnimating = false; // stop animation
-    }
-
-    // scroll init
-    initScrollBars();
-
-    scrollBar.mouseenter(function() {
-        scrollSetUp();
-    });
-
-    isiMain.mouseenter(function() {
-        stopScroll();
-    });
-
-    isiMain.mouseleave(function() {
-        if (animationFinished) {
-            if (!isiFinished) {
-                startScroll();
-            }
-        }
-    });
-
-    myScroll.on('scrollStart', function() {
-        if (animationFinished) {
-            if (myScroll.isAnimating) {
-                stopScroll();
-            }
-        }
-    });
-
-    myScroll.on('scrollEnd', function() {
-        if (!scrollToTop) return;
-        if (myScroll.maxScrollY >= myScroll.y) {
-            stopScroll();
-            setTimeout(function() {
-                myScroll.scrollTo(0, 0, 2000);
-            }, 3000);
-        }
-    });
-
-    //
-    // Timeline Animation
-    //
-
-    tl.addLabel('frame1', '+=0.5')
-        .from(placeholder, 0.6, { x: 500 }, 'frame1')
-        .add(function() {
-            finishedAnimation();
-        }, 'frame1+=3');
-
     // Exits Listeners
     mainExit.on('click', AppBanner.fn.mainExitHandler);
     $('#wrapper').on('click', AppBanner.fn.piExitHandler);
